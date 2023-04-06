c41615179ffa19e8bd42256e36f087f78d76ab42
ZARIFMAC <uzbzarif@gmail.com>
2023-04-06 15:02:09 +0500

    appbar

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Container(color: Colors.greenAccent),
+    home: Scaffold(
+      appBar: AppBar(),
+    ),
   ));
 }
