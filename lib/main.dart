00c7a24f700914792071584d19013042a31855e0
ZARIFMAC <uzbzarif@gmail.com>
2023-04-08 14:54:15 +0500

    expanded

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  Color gray = const Color(0xFFE0E7EC);
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold(
-      body: Column(
-        children: [
-          Container(
-            width: 50,
-            height: 50,
-            color: Colors.redAccent,
-          ),
-          Container(
-            width: 50,
-            height: 50,
-            color: Colors.greenAccent,
-          ),
-        ],
+      backgroundColor: const Color(0xFFF3F5F8),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.stretch,
+          // verticalDirection: VerticalDirection.up,
+          children: [
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.all(20),
+                width: 100,
+                // height: 200,
+                // color: gray,
+                decoration: BoxDecoration(
+                  color: gray,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            Container(
+              width: 150,
+              height: 200,
+              color: gray,
+            )
+          ],
+        ),
       ),
     ),
   ));
